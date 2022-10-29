f6a70ea63be01d87c2b2d7ec48b41b6c1ecda861
ShbairAli <ali89freelancer@gmail.com>
2022-10-29 07:17:29 +0300

    built full real dataset emulation for wms, leaving no missing rings, so that every single product life sycle is in its timeline and all elements are connected carefully

--- src/scripts/generateData.ts
+++ src/scripts/generateData.ts
@@ -13,13 +13,10 @@ const outputData = (fileName: string, data: string): void => {
 
 const users = JSON.stringify(DataGenerator.generateUsers(), null, 2);
 const products = JSON.stringify(DataGenerator.generateProducts(), null, 2);
-const transactions = JSON.stringify(
-  DataGenerator.generateTransactions(),
-  null,
-  2
-);
+const transactionsObj = DataGenerator.generateTransactions(1, 0, 50);
+const transactions = JSON.stringify(transactionsObj?.transactions, null, 2);
 const transactionsProducts = JSON.stringify(
-  DataGenerator.generateTransactionsProducts(),
+  transactionsObj?.transactionsProducts,
   null,
   2
 );
