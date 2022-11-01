ca1da44f1bc52875360f8bfa13dd9024b9e86401
ysalem-dev-89 <yazeed.salem.it@gmail.com>
2022-11-01 05:37:56 +0200

    changes on transactionProductInterface.ts

--- src/interfaces/TransactionProductInterface.ts
+++ src/interfaces/TransactionProductInterface.ts
@@ -1,11 +1,10 @@
-import { DecimalDataType } from 'sequelize';
 import { TransactionStatus } from './TransactionInterface';
 
 export interface TransactionProduct {
   id?: number;
   ProductId: number;
   status: TransactionStatus;
-  unitPrice: DecimalDataType;
+  unitPrice: number;
   quantity: number;
   createdAt: Date;
   updatedAt: Date;
